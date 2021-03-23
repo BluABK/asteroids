886678307c37076fdf486102f4f8691fb368ae47
BluABK <bluabk@bluabk.net>
2021-03-24 00:22:59 +0100

    Added asteroid (some cleanup pending).

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import {keys} from "./modules/keys.mjs";
 import {ship} from "./modules/ship/ship.mjs";
+import {asteroid} from "./modules/asteroid/asteroid.mjs";
 import {controlSchemeLoader} from "./modules/ship/controls.mjs";
 
 // // Settings items.
@@ -16,6 +17,8 @@ document.getElementById("main-menu-submit-button").addEventListener("click", sta
 // the window isn't guaranteed to be frontmost before this method returns.
 focus();
 
+const SVG_NS = "http://www.w3.org/2000/svg";
+
 let worldElement = document.getElementById("world-element");
 let worldSvgElement = document.getElementById("world-svg-element");
 // Controls avail: oldSchool, oldSchoolDrag, speedster, speedLimiter, engineRev
@@ -23,10 +26,23 @@ let playerShipControlScheme = null;
 let playerShipShape;
 let player;
 
+let myAsteroid;
+
 // Create spaceship shape in the SVG namespace.
 playerShipShape = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
 playerShipShape.setAttributeNS(null, "id", "spaceship");
 playerShipShape.setAttributeNS(null, "points", "0,0 -5,10 30,0 -5,-10");
+
+console.log("playerShipShape", playerShipShape);
+
+// let asteroidType2 = document.getElementById("svg-asteroid2").contentDocument.rootElement.children.layer1.children.path124; // 
+let asteroidType2 = document.createElementNS(SVG_NS, "path");
+asteroidType2.setAttributeNS(SVG_NS, "id", "asteroid2");
+asteroidType2.setAttribute("d", "M 13.229173,13.229166 7.9375033,11.90625 h -5.29167 L 3.3333333e-6,7.9375003 6.6145833,2.6458333 H 10.583333 L 11.906253,6.3333333e-7 17.197923,1.3229176 l 2.64583,3.9687497 5.29167,1.322917 1.32291,2.645833 -2.64583,2.6458327 -5.29167,1.322917 -5.29166,-1e-6");
+asteroidType2.setAttribute("fill", "white");
+// asteroidType2.setAttribute("transform", "scale(15.0)");
+asteroidType2.classList.add("asteroid");
+console.log("asteroidType2", asteroidType2);
 
 window.focus();
 
@@ -48,9 +64,48 @@ function createAndSpawnShip(shipShape) {
     return shipElement;
 }
 
+function clone(element){
+    let clonedElement = element.cloneNode(true);
+    // clonedElement.id="newPawn1"
+    worldSvgElement.appendChild(clonedElement)
+    console.log("clone()", element, clonedElement);
+    return clonedElement;
+}
+
+function createAndSpawnAsteroid(asteroidType, pos = {x: 0, y: 0}) {
+    // Add shape to the spaceship SVG container.
+    worldSvgElement.appendChild(asteroidType);
+
+    console.log("asteroidType", asteroidType);
+
+    // Make a copy of the template asteroid SVG.
+    // let asteroidClone = JSON.parse(JSON.stringify(asteroidType));
+    // let asteroidClone = clone(asteroidType);
+    // console.log("asteroidClone", asteroidClone);
+    // asteroidClone.setAttribute("pos", pos);
+    // asteroidClone.classList.remove("unspawned-object");
+    
+
+    // Add the asteroid.
+    // let asteroidTemplate = Object.assign({}, asteroidClone);
+    let asteroidTemplate = Object.assign({}, asteroidType);
+    console.log("asteroidTemplate", asteroidTemplate);
+
+    // Spawn the asteroid.
+    // let asteroidElement = asteroid.create(asteroidClone, pos);
+    let asteroidElement = asteroid.create(asteroidType, pos);
+
+    return asteroidElement;
+}
+
 function startGame() {
+    // Spawn player
     player = createAndSpawnShip(playerShipShape);
     console.log("Player spawned.", player);
+
+    // Spawn asteroids
+    myAsteroid = createAndSpawnAsteroid(asteroidType2, {x: 0.325, y: 1.0});
+    console.log("Asteroid spawned.", myAsteroid);
 
     // Tell the browser that we wish to perform an animation and request that the browser
     // call a specified function to update an animation before the next repaint.
@@ -64,6 +119,9 @@ function mainLoop(){
     // Update player.
     player.updateUserIO();
     player.updatePos();
+    
+    // Update Asteroids
+    myAsteroid.updatePos();
 
     // Request an animation frame with callback to self, so that it repeats/recurses infinitely.
     requestAnimationFrame(mainLoop);
---
+++ modules/asteroid/asteroid.mjs
@@ -0,0 +1,69 @@
+export const asteroid = {
+    element : null,
+    speed : 0,
+    speedC : 0,  // chase value for speed limit mode
+    speedR : 0,  // real value (real as in actual speed)
+    angle : 0,
+    angleC : 0,  // as above
+    angleR : 0,
+    engSpeed : 0,
+    engSpeedC : 0,
+    engSpeedR : 0,
+    displayAngle : 0, // the display angle
+    deltaAngle : 0,
+    matrix : null,  // matrix to create when instantiated 
+    pos : null,     // position of ship to create when instantiated 
+    delta : null,   // movement of ship to create when instantiated 
+    checkInView() {
+        var bounds = this.element.getBoundingClientRect();
+        if (Math.max(bounds.right,bounds.left) < 0 && this.delta.x < 0) {
+            this.pos.x = innerWidth;
+        } else if(Math.min(bounds.right,bounds.left) > innerWidth  && this.delta.x > 0) {
+            this.pos.x = 0;
+        }
+        if (Math.max(bounds.top,bounds.bottom) < 0  && this.delta.y < 0) {
+            this.pos.y = innerHeight;
+        } else if ( Math.min(bounds.top,bounds.bottom) > innerHeight  && this.delta.y > 0) {
+            this.pos.y = 0;
+        }
+        
+    },
+    /**
+     * Empty function to be replaced by a control scheme.
+     */
+    updateUserIO() {
+        this.controls(this);
+    },
+    updatePos() {
+        this.checkInView();
+        var m = this.matrix;
+        m[3] = m[0] = Math.cos(this.displayAngle);
+        m[2] = -(m[1] = Math.sin(this.displayAngle));
+        m[4] = this.pos.x;
+        m[5] = this.pos.y;
+
+        // this.element.style.transform = `matrix(${m.join(",")})`;
+        this.element.style.transform = `matrix(${m.join(",")}) scale(5.0)`;
+    },
+    /**
+     * Create new spaceship.
+     * 
+     * @param {HTMLElement} shape Shape of the spaceship.
+     * @param spawnOffset X, Y offset to spawn at.
+     * @param {float} spawnOffset.x X-Axis offset multiplier.
+     * @param {float} spawnOffset.y Y-Axis offset multiplier.
+     * @param {string} controlScheme {string} Ship model controls to map it to (must be one of: oldSchool, oldSchoolDrag, speedster, engineRev or speedLimiter).
+     * @returns {object} The created spaceship element.
+     */
+    create(shape, spawnOffset = {x: 0.325, y: 1}) {
+        this.element = shape;
+
+        // CSS Transform matrix
+        this.matrix = [1,0,0,1,0,0];
+        this.pos = { x : (innerWidth / 2 + innerWidth) * spawnOffset.x, y : (innerHeight / 2) * spawnOffset.y};
+        this.delta = { x : 0, y : 0};
+
+        // Return the newly created asteroid element.
+        return this;
+    }
+}
